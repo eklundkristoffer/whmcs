144cbb263fe37f08ec52ea15b55b8ed0c19030bf
Kristoffer Eklund <kristoffereklund1@gmail.com>
2016-11-26 02:47:02 +0100

    Small fix

--- install-mysql-ubuntu16.04.sh
+++ install-mysql-ubuntu16.04.sh
@@ -2,4 +2,3 @@
 sudo apt-get update
 sudo apt-get install mysql-server
 sudo mysql_secure_installation
-
--- install-php7-ubuntu16.04.sh
+++ install-php7-ubuntu16.04.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install php-fpm php-mysql
-sed -i '' -e 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.0/fpm/php.ini
+sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.0/fpm/php.ini
 sudo systemctl restart php7.0-fpm
-
-
-
